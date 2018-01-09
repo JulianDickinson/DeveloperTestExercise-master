80b3e84f056ad6c9b3d5b92c20c67f3a48133924
JulianDickinson <julian.dickinson@ic24.net>
2018-01-09 15:46:14 +0000

    Task1

---
+++ FileData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileDetails fileDets = new FileDetails();
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect number of arguments");
+                return;
+            }
+
+            if (args[0].ToString() == "-v")
+            {
+                Console.WriteLine(fileDets.Version(args[1].ToString()));
+                return;
+            }
+        }
+    }
+}
